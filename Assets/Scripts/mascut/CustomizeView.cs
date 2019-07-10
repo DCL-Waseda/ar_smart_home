8330cb94b7d1a23d83215bba06b3bf23c245fa9f
sigmasigma <kotagushima@gmail.com>
2019-07-10 16:52:33 +0900

    Made Customize Menu

---
+++ Assets/Scripts/mascut/CustomizeView.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pose=Thalmic.Myo.Pose;
+using UnityEngine.UI;
+
+public class CustomizeView : MonoBehaviour {
+
+	public GameObject myo;
+	ThalmicMyo thalmicMyo;
+	float cooltime=0;
+	bool input_right=true;
+	int target=0;
+	// Use this for initialization
+	void Start () {
+		thalmicMyo=myo.GetComponent<ThalmicMyo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!input_right){
+			cooltime--;
+			if(cooltime<0){
+				cooltime=0;
+				input_right=true;
+			}
+		}
+		//Fist();
+		WaveIn();
+		WaveOut();
+	}
+	void Fist(){
+		if(thalmicMyo.pose==Pose.Fist){
+			//Enter
+			Debug.Log("Fist");
+		}
+	}
+	void WaveIn(){
+		if(thalmicMyo.pose==Pose.WaveIn&&input_right){
+			//Left
+			if(target!=0){
+				Reset();
+				target-=1;
+				transform.GetChild(target).gameObject.GetComponent<Image>().color=new Color(1,1,1,1);
+			}
+		}
+	}
+	void WaveOut(){
+		if(thalmicMyo.pose==Pose.WaveOut&&input_right){
+			//Right
+			Debug.Log("Goright");
+			if(target!=3){
+				Reset();
+				target+=1;
+				transform.GetChild(target).gameObject.GetComponent<Image>().color=new Color(1,1,1,1);
+			}
+		}
+	}
+	void Reset(){
+		foreach (Transform child in transform)
+		{
+			child.GetComponent<Image>().color=new Color(1,1,1,0.2f);
+			input_right=false;
+			cooltime=60;
+		}
+	}
+}
---
+++ Assets/Scripts/mascut/GestureManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pose=Thalmic.Myo.Pose;
+
+public class GestureManager : MonoBehaviour {
+
+	ThalmicMyo thalmicMyo;
+	public GameObject myo;
+	// Use this for initialization
+	void Start () {
+		thalmicMyo=myo.GetComponent<ThalmicMyo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Fist();
+	}
+	void Fist(){
+		if(thalmicMyo.pose==Pose.Fist){
+			Debug.Log("Fist now");
+		}
+	}
+}
