c17a3647b436d7f7128c9ae93fc4fb4a15a32483
sorararan <masolainota@gmail.com>
2019-07-19 10:27:36 +0900

    配置しなおし

---
+++ Assets/Scripts/Common/Const.cs
@@ -0,0 +1,13 @@
+﻿namespace Display{
+	public class Const {
+		public static string POWER_ID = "c0035911-6e5d-43c5-a965-c11dcebcf924";
+    	public static string VOLUMEUP_ID  = "00db0c08-6677-410a-bb65-4f2edc7d47c8";
+    	public static string VOLUMEDOWN_ID = "9ca9ac8e-8739-4dad-9e75-bf1f8a93b597";
+	}
+}
+
+namespace NatureRemo{
+	public class Const{
+		public static string TOKEN = "J9f6s81LrC5Wjg1e93eqQSAE5HGNJ-AOgprVKVYFng0.O7D48CQKGUfuEiLBhe1-0VpVf_hxKPJ7cfZ6mPgXe3E";
+	}
+}
