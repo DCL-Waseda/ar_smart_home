be096fa33120cdc2cca25282dac791c65c8ae765
sorararan <masolainota@gmail.com>
2019-07-26 23:57:06 +0900

    ディスプレイオブジェクトairtapでボタン表示、ボタンairtapで家電操作

--- Assets/Scripts/Controllers/DisplayController.cs
+++ Assets/Scripts/Controllers/DisplayController.cs
@@ -4,24 +4,24 @@ using UnityEngine;
 
 public class DisplayController : Controller {
     DisplayApi api;
-    GameObject power_commander;
-    bool power_switch;
 
     void Start(){
         api = DisplayApi.instance;
-        power_commander = this.transform.Find("power").gameObject;
-        power_switch = false;
     }
     
-    void power(){
-        api.power();
-
-        if(power_switch){
-            power_commander.GetComponent<Renderer>().material.color = Color.white;
-        }else{
-            power_commander.GetComponent<Renderer>().material.color = Color.red;
+    void button_behavior(string method_name){
+        switch(method_name){
+            case "power": 
+                api.power();
+                break;
+            case "volume_up": 
+                api.volume_up();
+                break;
+            case "volume_down":
+                api.volume_down();
+                break;
+            default: 
+                break;
         }
-        power_switch = !power_switch;
     }
-
 }
