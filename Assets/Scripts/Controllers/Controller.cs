9866edae617aeb5ea1b58c85b1e5f39c1bbbf2e2
sorararan <masolainota@gmail.com>
2019-09-02 15:05:38 +0900

    myo_gesture_managerによるapi呼び出し

--- Assets/Scripts/Controllers/Controller.cs
+++ Assets/Scripts/Controllers/Controller.cs
@@ -1,17 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using HoloToolkit.Unity.InputModule;
 
-public class Controller : MonoBehaviour, IInputClickHandler {
-
-    // ボタンの表示
-    public void OnInputClicked(InputClickedEventData eventData) {
-        foreach (Transform childTransform in gameObject.transform){
-            childTransform.gameObject.SetActive(!childTransform.gameObject.activeSelf);
-        }
-    }
-
+public class Controller : MonoBehaviour {
     // 各家電APIインスタンスに操作名を投げる
     protected virtual void button_behavior(string method_name){
 
---
+++ Assets/Scripts/MyoGestureManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pose = Thalmic.Myo.Pose;
+
+public class MyoGestureManager : MonoBehaviour {
+		// Singltonパターン
+    private static MyoGestureManager m_Instance;
+    public static MyoGestureManager instance {
+        get {
+            if( m_Instance == null ) m_Instance = new MyoGestureManager();
+            return m_Instance;
+        }
+    }
+
+	  private GameObject FocusedObject;
+		private GameObject myo;
+		private Pose lastPose_ = Pose.Unknown;
+    private ThalmicMyo myo_;
+		
+		void Update() {
+				// 見ているオブジェクトの更新
+        RaycastHit hitInfo;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo)){
+            FocusedObject = hitInfo.collider.gameObject;
+        }else{
+            FocusedObject = null;
+        }
+
+				// myoジェスチャー認識
+				// 見ているオブジェクトからapi呼び出し
+				// 前回と比較して変化があればイベント発火
+				if(FocusedObject != null){
+						if (myo_.pose != lastPose_) {
+            		lastPose_ = myo_.pose;
+
+            		// ジェスチャに応じて処理を分ける
+            		switch (myo_.pose) {
+                		case Pose.Fist:
+                    		send_message(FocusedObject, "power");
+                    		break;
+               		 	case Pose.WaveIn:
+												send_message(FocusedObject, "volume_up");
+                    		break;
+                		case Pose.WaveOut:
+												send_message(FocusedObject, "volume_down");
+                    		break;
+                		case Pose.DoubleTap:
+												send_message(FocusedObject, "");
+                    		break;
+            		}
+        		}
+				}
+		}
+
+		private void send_message(GameObject target, string operation){
+			target.SendMessageUpwards("button_behavior", operation, SendMessageOptions.DontRequireReceiver);
+		}
+
+}
