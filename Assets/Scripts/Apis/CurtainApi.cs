ed34db382ae7b89deeaae982a3f5e720b4de806e
sorararan <masolainota@gmail.com>
2019-09-20 23:13:24 +0900

    fan、curtainのapi

---
+++ Assets/Scripts/Apis/CurtainApi.cs
@@ -0,0 +1,14 @@
+﻿using Curtain;
+
+public class CurtainApi : NatureRemoApi {
+
+    // Singltonパターン
+    private static CurtainApi m_Instance;
+
+    public static CurtainApi instance {
+        get {
+            if( m_Instance == null ) m_Instance = new CurtainApi();
+            return m_Instance;
+        }
+    }
+}
